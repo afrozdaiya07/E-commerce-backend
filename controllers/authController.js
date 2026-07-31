ec27285ce9882de44cc2b39cee3f9527e69c7e20
Afroz daiya07 <afrozdaiya07@gmail.com>
2026-07-31 09:04:00 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
